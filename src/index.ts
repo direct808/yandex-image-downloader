97509eeb399b80b7f083aae613560b551b83969d
direct808 <direct808@yandex.ru>
2018-12-21 18:05:47 +0300

    mw

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,64 @@
-import * as requestPromise from "request-promise";
+import minimist from "minimist";
+import {YIDownloader} from "./YIDownloader";
+import PQueue from "p-queue";
+import mkdirp from "mkdirp";
+
+const argv = minimist(process.argv.slice(2), {
+    default: {
+        c: 30,
+        i: 15,
+        t: 10,
+        o: 1,
+    }
+});
+
+let interval = argv.i as number;
+let count = argv.c as number;
+let offset = argv.o as number;
+let queryString = argv._.join(" ");
+
+if (queryString.length == 0) {
+    process.exit();
+}
+
+let directory = "./" + queryString + "/";
+mkdirp.sync(directory);
+
+let queue = new PQueue({concurrency: argv.t});
+let downloader = new YIDownloader(interval);
+
+let index = 1;
+let pageStart = Math.ceil(offset / 30);
+let pageEnd = Math.ceil((count + offset - 1) / 30);
+let curIndex = (pageStart - 1) * 30;
+
+console.log("Download", count, "photos", "Start with", offset, "Query string:", queryString);
+// console.log("pageStart", pageStart, "pageEnd", pageEnd);
+
+
+(async () => {
+    for (let page = pageStart; page <= pageEnd; page++) {
+        let urls = await downloader.getList(queryString, page);
+        console.log("page", page);
+        if (urls.length == 0) {
+            throw new Error("Empty result");
+        }
+        for (let url of urls) {
+            curIndex++;
+            if (curIndex < offset) {
+                continue;
+            }
+            ((curIndex) => {
+                queue.add(() => downloader.downloadImage(directory + curIndex + ".jpg", url)
+                    .then(() => console.log("#" + curIndex + " ok " + url)))
+                    .catch(e => console.error("#" + curIndex + " " + e.message + " " + url));
+            })(curIndex);
+            if (index >= count) {
+                return;
+            }
+            index++;
+        }
+    }
+})();
+
+export {YIDownloader};
--- test/Downloader.spec.ts
+++ test/Downloader.spec.ts
@@ -1,23 +1,23 @@
-import {Downloader} from "../src/Downloader";
+import {YIDownloader} from "../src/YIDownloader";
 import PQueue from "p-queue";
 
-describe("Downloader", function () {
+describe("YIDownloader", function () {
     this.timeout(Infinity);
 
     // it('getListPage', async function () {
-    //     let sl = new Downloader();
+    //     let sl = new YIDownloader();
     //     let urls = await sl.getListPage("Фото девушек 25 лет", 2);
     //     // console.log(urls);
     // });
 
     it('getList', async function () {
-        let sl = new Downloader();
+        let sl = new YIDownloader();
         let urls = await sl.getList("Фото девушек 25 лет");
         console.log(urls);
     });
 
     it('download', async function () {
-        let sl = new Downloader();
+        let sl = new YIDownloader();
         await sl.download("https://i08.fotocdn.net/s25/206/gallery_m/377/2624288973.jpg");
         // console.log(urls);
     });
@@ -27,7 +27,7 @@ describe("Downloader", function () {
         let queue = new PQueue({
             concurrency: 10,
         });
-        let sl = new Downloader();
+        let sl = new YIDownloader();
 
         for (let page = 1; page <= 20; page++) {
             let urls = await sl.getList("фото парней 25 лет ok", page);
